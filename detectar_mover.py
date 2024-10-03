d8b3708581fae1d73964efe8e7fecf8e37b6637e
patricionarbona <patricioinarbona@gmail.com>
2024-10-03 17:50:36 +0200

    add detect elements

--- detectar_mover.py
+++ detectar_mover.py
@@ -1,14 +1,36 @@
 import pyautogui
 import time
 
-inicio = [1457,884]
+# inicio = [1626,614]
 
-# while True:
-#     mouseX, mouseY = pyautogui.position()
-#     print('x: ',mouseX, ' y: ', mouseY)
-#     time.sleep(5)
+# # while True:
+# #     mouseX, mouseY = pyautogui.position()
+# #     print('x: ',mouseX, ' y: ', mouseY)
+# #     time.sleep(5)
 
-pyautogui.moveTo(inicio[0], inicio[1])
-time.sleep(0.5)
-# pyautogui.drag(1000, 0, button='left', duration=0.1)
+# pyautogui.moveTo(inicio[0], inicio[1])
+# time.sleep(0.5)
+# # pyautogui.drag(1000, 0, button='left', duration=0.1)
+# pyautogui.dragRel(500, 0, duration=0.2)
+# time.sleep(5)
+# pyautogui.click()
+
+buttonDrag = pyautogui.locateOnScreen('./muestras/buttonDrag.png', confidence=0.9)
+print(buttonDrag)
+buttonDragCenter = pyautogui.center(buttonDrag)
+print(buttonDragCenter.x)
+pyautogui.moveTo(buttonDragCenter.x, buttonDragCenter.y)
 pyautogui.dragRel(500, 0, duration=0.2)
+pyautogui.click()
+
+buttonDrag = pyautogui.locateOnScreen('./muestras/buttonShort.png', confidence=0.9)
+print(buttonDrag)
+buttonDragCenter = pyautogui.center(buttonDrag)
+print(buttonDragCenter.x)
+pyautogui.moveTo(buttonDragCenter.x, buttonDragCenter.y)
+
+buttonDrag = pyautogui.locateOnScreen('./muestras/buttonLong.png', confidence=0.9)
+print(buttonDrag)
+buttonDragCenter = pyautogui.center(buttonDrag)
+print(buttonDragCenter.x)
+pyautogui.moveTo(buttonDragCenter.x, buttonDragCenter.y)
