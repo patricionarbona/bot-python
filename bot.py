a55ea552e7c3c08eb8991280ba78feabb6c0de8a
patricioinarbona@gmail.com <patricioinarbona@gmail.com>
2024-11-01 15:49:13 +0100

    add detec_value

--- bot.py
+++ bot.py
@@ -18,6 +18,7 @@ stop_requested = False  # Variable global para detener el bucle
 val_mode = "numbers"
 val_max = 1000
 val_min = 20
+reader = easyocr.Reader(['es']) 
 
 def submit_percentage():
     global val_max, val_min, val_mode
@@ -139,6 +140,101 @@ def cerrar_operacion():
     pyautogui.moveTo(buttonDragCenter.x, buttonDragCenter.y)
     pyautogui.click()
 
+def control_increment(value):
+    print(value)
+
+#Detectar valor
+def detec_value(op_open):
+    negativo = False
+    buttonDrag = pyautogui.locateOnScreen('./muestras/percentReference.png', confidence=0.8)
+    if buttonDrag is not None:
+        buttonDragCenter = pyautogui.center(buttonDrag)
+        print(buttonDragCenter.x, buttonDragCenter.y)
+        
+        # Mueve el cursor a la posición del botón
+        # pyautogui.moveTo(buttonDragCenter.x, buttonDragCenter.y)
+        # time.sleep(1)
+
+        # Define el ancho y alto para la captura
+        width, height = 300, 40  # Cambia estos valores según tus necesidades
+
+        # Calcula las coordenadas del área a capturar
+        left = int(buttonDragCenter.x - width // 2) - 165
+        top = int(buttonDragCenter.y - height // 2)
+
+        # Verifica que las coordenadas no sean negativas
+        if left < 0:
+            left = 0
+        if top < 0:
+            top = 0
+
+        # Obtiene la resolución de la pantalla
+        screenWidth, screenHeight = pyautogui.size()
+        
+        # Asegúrate de que las coordenadas y el tamaño de la región estén dentro de los límites de la pantalla
+        if left + width > screenWidth:
+            width = screenWidth - left
+        if top + height > screenHeight:
+            height = screenHeight - top
+
+        # Realiza la captura de pantalla
+        screenshot = pyautogui.screenshot(region=(left, top, width, height))
+        
+        # Convierte la imagen a formato OpenCV
+        screenshot_cv = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+
+        # Analiza el color en la región donde se espera el texto
+        # Define un rango para los colores
+        lower_red = np.array([0, 0, 100])
+        upper_red = np.array([50, 50, 255])
+        lower_green = np.array([0, 100, 0])
+        upper_green = np.array([50, 255, 50])
+
+        mask_red = cv2.inRange(screenshot_cv, lower_red, upper_red)
+        mask_green = cv2.inRange(screenshot_cv, lower_green, upper_green)
+
+        red_count = cv2.countNonZero(mask_red)
+        green_count = cv2.countNonZero(mask_green)
+
+        # Determina si el número es negativo o positivo
+        if red_count > green_count:
+            print("El número es negativo")
+            negativo = True
+        elif green_count > red_count:
+            print("El número es positivo")
+            negativo = False
+        else:
+            print("No se pudo determinar el signo del número")
+
+        # reader = easyocr.Reader(['es']) 
+        result = reader.readtext(screenshot_cv)
+
+        # Imprime los resultados
+        for (bbox, text, prob) in result:
+            print(f'Texto: {text} (Confianza: {prob:.2f})')
+            # print(text.split())
+            # print(text.split()[0]) #value
+            # print(text.split('(')[1].replace(')',''))# percent
+            if(val_mode == "percent"):
+                num = text.split()[2].replace(')','').replace('%','').replace('(','')
+                if negativo:
+                    num = num.replace('-','')
+                    num = '-' + num
+                control_increment(num)
+            if(val_mode == "numbers"):
+                num = text.split()[0]
+                if negativo:
+                    num = num.replace('-','')
+                    num = '-' + num
+                control_increment(num)
+
+
+
+
+
+        # Muestra la imagen usando OpenCV
+        # cv2.imshow('Captura', screenshot_cv)
+
 # Fase 0: Sin Operaciones
 def fase_sin_operaciones(resultado):
     global fase
@@ -164,6 +260,7 @@ def fase_long_abierto(resultado):
 # Fase 2: Espera confirmación Long Cerrado
 def fase_espera_confirmacion_long(resultado):
     global fase
+    detec_value("long")
     for res in resultado:
         if res[1] == '51':  # Si se reconoce "51", cerramos Long
             print('Confirmación - Long Cerrado')
@@ -185,6 +282,7 @@ def fase_short_abierto(resultado):
 # Fase 4: Espera confirmación Short Cerrado
 def fase_espera_confirmacion_short(resultado):
     global fase
+    detec_value("short")
     for res in resultado:
         if res[1] == 'B1':  # Si se reconoce "B1", abrimos de nuevo Long
             print('Confirmación - Short Cerrado')
